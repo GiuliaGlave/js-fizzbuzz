5937047515a056ae68afe9846d212ef4f3b04605
GiuliaGlave <giulia.glave@gmail.com>
2023-01-25 19:22:28 +0100

    add background color multx3 x5

--- script.js
+++ script.js
@@ -2,6 +2,31 @@
 const gridEl = document.getElementById("grid")
 
 for (let i = 1; i <= 100; i++) {
-    gridEl.innerHTML += `<li class="box box–-${i}">${i}</li>`;
+/*     gridEl.innerHTML += `<li class="box box–-${i}">${i}</li>`; */
+ 
+//multipli di 3  
+
+    let gridClass;
+
+    if(i % 3 == 0){
+        gridClass = "box box-multi-3 ";
+    }
+
+    else if (i % 5 == 0) {
+        gridClass = "box box-multi-5";
+    }
+
+
+    else{
+        gridClass = `box box–-${i}`
+    }
+    
+    gridEl.innerHTML += `<li class="${gridClass}">${i}</li>`;
     
 }
+
+
+
+//multipli di 5
+
+//multipli di 15
